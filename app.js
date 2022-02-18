f71e0ceb4a18db9edd9f88d2dd9141e495e6b0ee
icesplendent <icesplendent@gmail.com>
2022-02-18 13:38:06 +0800

    feat: add libs/config & logger

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const config = require("./libs/config");
+const logger = require("./libs/logger");
 require("dotenv/config");
 
 const app = new express();
 
-// PORT
-const PORT = process.env.PORT || 3000;
-
-//GET
-app.get("/", (req, res) => {
-    res.send(`Hello from port ${PORT}`);
-});
-
 // Connect to MongoDB
-mongoose.connect(PORT, () => {
+mongoose.connect(process.env.DB_CONNECTION, () => {
     console.log("Connect to DB");
 });
 
-app.listen(PORT);
+//GET
+app.get("/", (req, res) => {
+    res.send({ msg: "Hello from port " + config.port });
+});
+
+app.listen(config.port, () => {
+    logger.info(`Server is running at port ${config.port}`);
+});
