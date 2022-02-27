eeea26eff4c642ccc720501dd4ee57365e040f53
ARui <chen108060030@gapp.nthu.edu.tw>
2022-02-27 15:13:01 +0800

    feat: finish photo apis

--- controller/photo.js
+++ controller/photo.js
@@ -2,26 +2,34 @@ import service from '../service';
 import logger from '../libs/logger';
 import validator from '../libs/validator';
 
+const idRule = {
+  type: 'multi',
+  rules: [{ type: 'string' }, { type: 'object' }],
+};
+const photoRule = {
+  fileName: {
+    type: 'string',
+    empty: false,
+  },
+  fileType: {
+    type: 'enum',
+    values: ['FrontPage', 'Shirt'],
+  },
+  description: {
+    type: 'string',
+    empty: false,
+  },
+};
+
 const photoController = {
   async upload(req, res) {
-    const rule = {
-      fileName: {
-        type: 'string',
-        empty: false,
-      },
-      fileType: {
-        type: 'enum',
-        values: ['FrontPage', 'Shirt'],
-      },
-      description: {
-        type: 'string',
-        empty: false,
-      },
-    };
     const { filename: fileName, filetype: fileType } = req.headers;
     let { description } = req.headers;
 
-    if (fileType === 'Shirt' && (description === '' || description === undefined)) {
+    if (
+      fileType === 'Shirt'
+      && (description === '' || description === undefined)
+    ) {
       description = 'Shirt Size Image';
     }
 
@@ -33,12 +41,70 @@ const photoController = {
     };
 
     try {
-      validator.validate({ fileName, fileType, description }, rule);
+      validator.validate({ fileName, fileType, description }, photoRule);
       const savedImage = await service.photo.uploadOne(params);
       res.send(savedImage);
     } catch (error) {
       logger.error('[Photo Controller] Failed to upload photo:', error);
       res.status(400).json({ message: `Failed to upload photo, ${error}` });
+    }
+  },
+  async getPhoto(req, res) {
+    const rule = {
+      _id: idRule,
+    };
+
+    try {
+      validator.validate(req.body, rule);
+      const user = await service.photo.findOne(req.body);
+      res.json(user);
+    } catch (error) {
+      logger.error('[Photo Controller] Failed to getPhoto:', error);
+      // TODO: remove error
+      res.status(400).json({ message: `Failed to getPhoto, ${error}` });
+    }
+  },
+  async getPhotos(req, res) {
+    const rule = {
+      filter: {
+        type: 'object',
+        optional: true,
+      },
+      limit: {
+        type: 'number',
+        optional: true,
+      },
+      skip: {
+        type: 'number',
+        optional: true,
+      },
+      sort: {
+        type: 'object',
+        optional: true,
+      },
+    };
+
+    try {
+      validator.validate(req.body, rule);
+      const photos = await service.photo.findAll(req.body);
+      res.json(photos);
+    } catch (error) {
+      logger.error('[Photo Controller] Failed to getPhotos:', error);
+      res.status(400).json({ message: `Failed to getPhotos, ${error}` });
+    }
+  },
+  async removePhoto(req, res) {
+    const rule = {
+      _id: idRule,
+    };
+
+    try {
+      validator.validate(req.body, rule);
+      const result = await service.photo.deleteOne(req.body);
+      res.json(result);
+    } catch (error) {
+      logger.error('[Photo Controller] Failed to removePhoto:', error);
+      res.status(400).json({ message: `Failed to removePhoto, ${error}` });
     }
   },
 };
--- service/photo.js
+++ service/photo.js
@@ -3,21 +3,20 @@ import { v4 as uuidv4 } from 'uuid';
 import model from '../models';
 import logger from '../libs/logger';
 
-const webService = {
+const photoService = {
   async uploadOne(params) {
     try {
-      //   const photo = await model.Photo.create(param);
       const {
         data, fileName, fileType, description,
       } = params;
       const rootDir = process.cwd();
       const path = `/public/${uuidv4()}_${fileName}`;
       fs.writeFileSync(`${rootDir}${path}`, data);
-      model.Photo.create({
+      const result = model.Photo.create({
         name: fileName, type: fileType, description, path,
       });
       logger.info('[Photo Service] Upload one success');
-      return true;
+      return result;
     } catch (error) {
       logger.error(
         '[Photo Service] Failed to upload image to database:',
@@ -26,6 +25,53 @@ const webService = {
       throw new Error(`Failed to create web database, ${error}`);
     }
   },
+  async findOne(filter) {
+    try {
+      const result = await model.Photo.findOne(filter).lean();
+      logger.info('[Photo Service] Find photo successfully');
+      return result;
+    } catch (error) {
+      logger.error('[Photo Service] Failed to find photo in database', error);
+      throw new Error(`Failed to find photo in database, ${error}`);
+    }
+  },
+  async findAll(params) {
+    const {
+      filter, limit, skip, sort = { order: -1 },
+    } = params;
+
+    try {
+      const total = await model.Photo.countDocuments(filter).lean();
+      const data = await model.Photo.find(filter, null, { limit, skip, sort }).lean();
+      logger.info('[Photo Service] Find photos successfully');
+      return { total, data };
+    } catch (error) {
+      logger.error('[Photo Service] Failed to find photos in database', error);
+      throw new Error(`Failed to find photos in database, ${error}`);
+    }
+  },
+  // async updateOne(params) {
+  //   const { _id } = params;
+
+  //   try {
+  //     const result = await model.Photo.updateOne({ _id }, params).lean();
+  //     logger.info('[Photo Service] Update photo successfully');
+  //     return result.modifiedCount > 0 ? { success: true } : { success: false };
+  //   } catch (error) {
+  //     logger.error('[Photo Service] Failed to update photo in database', error);
+  //     throw new Error(`Failed to update photo in database, ${error}`);
+  //   }
+  // },
+  async deleteOne(filter) {
+    try {
+      const result = await model.Photo.deleteOne(filter).lean();
+      logger.info('[Photo Service] Delete photo successfully');
+      return result.deletedCount > 0 ? { success: true } : { success: false };
+    } catch (error) {
+      logger.error('[Photo Service] Failed to delete photo in database', error);
+      throw new Error(`Failed to delete photo in database, ${error}`);
+    }
+  },
 };
 
-export default webService;
+export default photoService;
